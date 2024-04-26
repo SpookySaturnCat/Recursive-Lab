1b6b3f000e8d49bbc15aa8bc66ef6c863ae30642
Seraphina <124868030+SpookySaturnCat@users.noreply.github.com>
2024-04-26 13:43:17 -0400

    Add files via upload

--- Lab12Prob03.java
+++ Lab12Prob03.java
@@ -1,41 +1,26 @@
+
 /**
- * File: Lab11ONealMorrison.java 
- * Class: CSCI 1302 
- * Author: Taylor O'Neal, Seraphina Morrison 
- * Created on: April 19th, 2024 Last Modified: April 19th,
+ * File: Lab11ONealMorrison.java Class: CSCI 1302 Author: Taylor O'Neal,
+ * Seraphina Morrison Created on: April 19th, 2024 Last Modified: April 19th,
  * 2024 Description:
  */
-
 public class Lab12Prob03 {
+
 	public static void main(String[] args) {
 		String string = "hello";
 		System.out.println(reverseString(string));
-				
 	}
-	public static int function(int index) {
-		if (index == 0) {
-			return 1;
-		} else if (index == 1) {
-			return 3;
-		} else if (index == 2) {
-			return 4;
-		} else {
-			return function(index - 1) * (function(index - 1) - function(index - 2));
-		}
-	}
-	
+
 	public static String reverseString(String string) {
-		String newString = "";
-		int length = string.length();
-		newString = reverseString(string, length);
-		return newString;
+		return reverseString(string, string.length() - 1);
 	}
 	
-	public static String reverseString(String string, int index) {
-		if (index == 1) {
-			return string;
+	public static String reverseString(String s, int n) {
+		if (n <= 0) {
+			return "" + s.charAt(0);
 		} else {
-			return string.charAt(string.length() - 1) + reverseString(string.substring(0, index - 1));
+			return (s.charAt(n) + reverseString(s, n - 1));
 		}
 	}
+
 }
